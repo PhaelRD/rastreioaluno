e6a290fe7590f3bb8adefd0bdcf55c7e40719960
PhaelRD <69764002+PhaelRD@users.noreply.github.com>
2024-08-25 20:30:54 -0300

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-analytics")
 
-    implementation ("com.google.android.material:material:1.8.0")
+    implementation ("com.google.android.material:material:1.12.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
